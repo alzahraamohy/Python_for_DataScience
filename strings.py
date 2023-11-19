f2cbc51248c295f21833efe8ec5b83834372f46e
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 12:46:48 +0200

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,17 @@
+-- The string can be in single quotes or double ' "
+"Zahra" 'Zahra'
+-- Each element in string has index
+zahra[0]    #z
+zahra[1:3]  #ahr
+zahra[::2]  #zha appear the element +2 in the whole str
+zahra[:3:2]  #zh the same but only in the first 3 indexs
+
+-- to know the length of string use len
+len("zahra")   #5 , it also count spaces
+
+-- we can combine the strings 
+x = "zahra"
+y = x + "can code"    #"zahra can code"
+
+-- Tubles - by applying any operation on string 
+3 * zahra    #"zahra zahra zahra"
