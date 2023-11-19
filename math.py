9207723d4c91f31826aaa1f41035355da8392b00
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 11:45:18 +0200

    Update math.py

--- math.py
+++ math.py
@@ -1,3 +1,13 @@
 --if we have 45 + 5 + 2 + 1 , then we call the numbers "operands" and the addictive symbol "operator"
 --python can do different math operations ( + , - , * , /) and so on.. , "//" is for integer division ex: 25//6 = 4 but 25/6 = 4.166
+--python follows the mathematical conventions (precendence), like multiplying before adding and so on..
+  
+--when we assign a value to a variable A we use "=" operator
+  A = 5
+-- but if we want to replace the valuse anytime in the code we use ":" operator , and the first value is missed
+  A : 8
+-- we can perform operations on A
+  y = 2 * 5
+  y : A / 2
+-- 
 
