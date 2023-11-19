025ff3e3d5db6dba156da630bb68242554b65807
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 11:04:03 +0200

    starting.py

--- starting.py
+++ starting.py
@@ -5,6 +5,10 @@ print("Hello world")
 
 #we have different datatypes
 int x
-flout y
+float y
+#to know what the type of anything
+type(5)
 #to casting from int to float
 float(2):2.0
+int('1'):1
+int('A') #result error
