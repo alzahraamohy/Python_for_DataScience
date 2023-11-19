fb45e51015cea48bf2eae2f1919c28af70ffa02e
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 14:28:53 +0200

    Update 4-Lists_and_Tuples.py

--- 4-Lists_and_Tuples.py
+++ 4-Lists_and_Tuples.py
@@ -1,1 +1,20 @@
+-- Tubles are ordered lists , comma separated, can be float or int or string
+  Ratings = ( 10 , 100 , 3 , 5 )
 
+-- we can contain the all types in one tuble
+  x = ('zahra' , 3.88 , 1 )
+
+-- we can access all elements by :
+  x [0]  #'zahra'
+  x [2]  #1
+
+-- we can access a range
+  x[0:2] #'zahra' , 3.88 , 1 
+
+-- we can combine tubles by ' + '
+
+-- we can use len to indicate the length of the tuple
+
+-- if we want to sort a tuple we can use sorted function
+  new = sorted(x)
+-- 
