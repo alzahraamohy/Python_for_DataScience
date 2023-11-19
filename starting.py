fc6bed85e53b647b045ec3a4ab0e537d4fb3daed
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 11:00:51 +0200

    starting.py

---
+++ starting.py
@@ -0,0 +1,10 @@
+#use "#" to make comments
+#To display any string use print
+print("Hello world")
+#python do not use semicolomns.
+
+#we have different datatypes
+int x
+flout y
+#to casting from int to float
+float(2):2.0
