fa50dd5bd75506079e513db59b86a4215503d845
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 11:11:14 +0200

    Create math.py

---
+++ math.py
@@ -0,0 +1,2 @@
+--if we have 45 + 5 + 2 + 1 , then we call the numbers "operands" and the addictive symbol "operator"
+--python can do different math operations ( + , - , * , /) and so on.. , "//" is for integer division ex: 25//6 = 4
