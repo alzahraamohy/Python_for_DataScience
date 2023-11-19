50b648d49502139acbc1dd0ee356cda332b167ed
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-11-19 12:29:38 +0200

    Update math.py

--- math.py
+++ math.py
@@ -2,20 +2,20 @@
 --python can do different math operations ( + , - , * , /) and so on.. , "//" is for integer division ex: 25//6 = 4 but 25/6 = 4.166
 --python follows the mathematical conventions (precendence), like multiplying before adding and so on..
   
---when we assign a value to a variable A we use "=" operator
+--when we assign a value to a variable A we use "=" operator and we can change it anytime and apply operations
   A = 5
--- but if we want to replace the valuse anytime in the code we use ":" operator , and the first value is missed
-  A : 8
--- we can perform operations on A
   y = 2 * 5
-  y : A / 2
--- # Check the Python Version
+  y = A / 2
 
+-- # Check the Python Version
 import sys
 print(sys.version)
-# Print string and error to see the running order
 
+# Print string and error to see the running order
 print("This will be printed")
 frint("This will cause an error")
 print("This will NOT be printed")
+#The third print statement is not executed because the second line contains an error. 
 
+# System settings about float type
+sys.float_info
