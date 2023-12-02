8ed165a748bf7a7b616ad8adbce2fd2a3339bae4
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-12-02 15:51:30 +0200

    Update 4-Lists_and_Tuples.py

--- 4-Lists_and_Tuples.py
+++ 4-Lists_and_Tuples.py
@@ -1,12 +1,36 @@
--- Tubles are ordered lists , comma separated, can be float or int or string
-  Ratings = ( 10 , 100 , 3 , 5 )
+-- Tubles are containers of ( strings , floats, intgers )
 
 -- we can contain the all types in one tuble
-  x = ('zahra' , 3.88 , 1 )
+    x = ('zahra' , 3.88 , 1 )
 
--- we can access all elements by :
-  x [0]  #'zahra'
-  x [2]  #1
+-- Tubles represented by " ( ) ".
+    # Create your first tuple
+    tuple1 = ("disco",10,1.2 )
+
+-- Each element of a tuple can be accessed via an index.
+    x [0]  #'zahra'
+    x [2]  #1
+    # Print the variable on each index
+    print(tuple1[0])  #'disco'
+    print(tuple1[1])  #10
+    print(tuple1[2])  #1.2
+
+-- We can know the type of eah element by 
+    # Print the type of value on each index
+    print(type(tuple1[0]))      #<class 'str'>
+    print(type(tuple1[1]))      #<class 'int'>
+    print(type(tuple1[2]))      #<class 'float'>
+
+-- We can also use negative indexing 
+    tuple1[-1]    #1.2
+    
+-- We can concatenate or combine tuples by using the + sign
+    tuple2 = tuple1 + ("hard rock", 10)   
+    # ('disco', 10, 1.2, 'hard rock', 10)
+
+-- We can slice tuples, obtaining new tuples with the corresponding elements
+    tuple2[0:3]    #('disco', 10, 1.2)
+    # Note : we said from 0 to 3, not form 0 to 2 , we add increase extra 1
 
 -- we can access a range
   x[0:2] #'zahra' , 3.88 , 1 
