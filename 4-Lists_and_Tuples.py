d00e4925aac69dd5afece06b7141a3b4abcabe76
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2023-12-02 16:06:20 +0200

    4-Tuples.py

--- 4-Lists_and_Tuples.py
+++ 4-Lists_and_Tuples.py
@@ -32,6 +32,31 @@
     tuple2[0:3]    #('disco', 10, 1.2)
     # Note : we said from 0 to 3, not form 0 to 2 , we add increase extra 1
 
+-- We can obtain the length of a tuple using the length command
+    len(tuple2)         #5
+
+-- We can sort the values in a tuple and save it to a new tuple
+    Ratings = (0, 9, 6, 5, 10, 8, 9, 6, 2)
+    RatingsSorted = sorted(Ratings)
+    RatingsSorted         #[0, 2, 5, 6, 6, 8, 9, 9, 10]
+
+-- Nested Tuple
+
+    A tuple can contain another tuple as well as other more complex data types. This process is called 'nesting'. 
+    NestedT =(1, 2, ("pop", "rock") ,(3,4),("disco",(1,2))) 
+    # Print element on each index, including nest indexes
+
+    print("Element 2, 0 of Tuple: ",   NestedT[2][0])    #Element 2, 0 of Tuple:  pop
+    print("Element 4, 1 of Tuple: ",   NestedT[4][1])    #Element 4, 1 of Tuple:  (1, 2)
+
+    # Print the first element in the second nested tuples
+
+    NestedT[2][1][0]    #'r'
+
+    #  Print the second element in the second nested tuples
+
+    NestedT[2][1][1]    #'o'
+
 -- we can access a range
   x[0:2] #'zahra' , 3.88 , 1 
 
