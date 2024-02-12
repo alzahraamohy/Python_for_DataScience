2663e0869431ca1fbe19fbe71ee06e49cd440aa5
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2024-02-12 18:28:09 +0200

    Create 015-Gaussian_Distribution.py

---
+++ 015-Gaussian_Distribution.py
@@ -0,0 +1,17 @@
+https://www.udacity.com/course/intro-to-statistics--st101
+
+
+-- what we can do ?
+1) Read in dataset
+2) Calculate mean
+3) Calculate standard deviation
+4) Plot histogram
+5) Plot probability density function
+6) add two gaussian distributions
+
+
+
+
+
+
+
