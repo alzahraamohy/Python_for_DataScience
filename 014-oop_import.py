df663f1a636bf68193220b4ab55ebf36eab2ef7f
Alzahraa Mohy <65045953+alzahraamohy@users.noreply.github.com>
2024-02-12 18:14:41 +0200

    Create 014-oop_import.py

---
+++ 014-oop_import.py
@@ -0,0 +1,6 @@
+-- To include a function named Shirt inside a file named shirt.py to another file named example
+we should write this single line of code and do wharever we want
+
+>>> from shirt import Shirt
+
+
